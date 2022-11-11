1ed0a380784af62159c24c73890b1d3c568efaa1
Omargf16 <legends.gonzalez@gmail.com>
2022-11-11 15:07:14 +0100

    main

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("receta pizza");
+const harina = " harina"
+let harinaGramos = " 400 gramos de harina"
+const agua = " Agua"
+let mililitrosAgua = " 200ml de agua tibia"
+const oliva = " aceite de oliva"
+let aceite = " 2 cucharadas de aceite"
+const sal = " sal"
+let cantidadSal = " 1 pizca de sal"
+const levadura = " levadura"
+let levaduraGramos = " 15 gramos de levadura"
+const tomate = " tomate"
+let tomateSalsa = " 200 gramos de salsa de tomate"
+const cebolla = " cebolla caramelizada"
+let caramelizadaCebolla = " 75 gramos de cebolla"
+const bacon = " bacon"
+let gramosBacon = " 50 gramos de bacon"
+const quesoCabra = " queso de cabra"
+let quesoCabraGramos = " 75 gramos de queso de cabra"
+const quesoMozzarella = " queso mozzarella"
+let gramosQuesoMozzarella = " 100 gramos de queso mozzarella"
+console.log ("ingredientes")
+console.log("hacer una montaña con" + harina + ", una" + sal + ", y" + levadura)
+console.log(" ")
